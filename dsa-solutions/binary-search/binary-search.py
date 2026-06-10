ba7bbe92ad2afb60bf12e366ca4e8ea42527a4dc
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2026-06-10 13:34:21 -0600

    Add binary-search.py

---
+++ dsa-solutions/binary-search/binary-search.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                return m
+        return -1
+
+
+# Time Complexity: O(log(n))
+# Space Complexity: O(1)

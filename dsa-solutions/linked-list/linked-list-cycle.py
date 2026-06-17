a20bd17c4f6c385f23c2ef09edc51ce25ae620bf
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2026-06-17 12:34:16 -0600

    Add linked-list-cycle.py

---
+++ dsa-solutions/linked-list/linked-list-cycle.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+class ListNode:
+    val: int = 0
+    next: ListNode = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+        return False

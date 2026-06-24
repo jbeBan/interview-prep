886ad9178af3daa680d6167ecdfe54c6192ca700
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2026-06-24 11:56:54 -0600

    Add invert-binary-tree.py

---
+++ dsa-solutions/trees/invert-binary-tree.py
@@ -0,0 +1,26 @@
+from collections import deque
+from dataclasses import dataclass
+from typing import Optional
+
+
+class TreeNode:
+    val: int = 0
+    left: TreeNode = None
+    right: TreeNode = None
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        queue = deque([root]) if root else deque()
+        while queue:
+            curr = queue.pop()
+            if curr.left:
+                queue.appendleft(curr.left)
+            if curr.right:
+                queue.appendleft(curr.right)
+            curr.left, curr.right = curr.right, curr.left
+        return root
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)

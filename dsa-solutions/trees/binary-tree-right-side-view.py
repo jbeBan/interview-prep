48741b796d9293a3b2adf3f400a383665d1077d9
Jacob Banuelos <72123087+jbeBan@users.noreply.github.com>
2026-09-14 15:45:29 -0600

    Add binary-tree-right-side-view.py

---
+++ dsa-solutions/trees/binary-tree-right-side-view.py
@@ -0,0 +1,30 @@
+from collections import deque
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class TreeNode:
+    val: int = 0
+    left: TreeNode = None
+    right: TreeNode = None
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        rs = []
+        q = deque([root]) if root else deque([])
+        while q:
+            l = len(q)
+            for i in range(l):
+                n = q.pop()
+                if i == l - 1:
+                    rs.append(n.val)
+                if n.left:
+                    q.appendleft(n.left)
+                if n.right:
+                    q.appendleft(n.right)
+        return rs
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)

192e84532d8c1eea3c1794d0d60b9c505ab3fca0
Jacob Banuelos <72123087+jbeBan@users.noreply.github.com>
2026-09-15 13:36:21 -0600

    Add count-good-nodes-in-binary-tree.py

---
+++ dsa-solutions/trees/count-good-nodes-in-binary-tree.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class TreeNode:
+    val: int = 0
+    left: TreeNode = None
+    right: TreeNode = None
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def good_nodes(n: TreeNode, m: int) -> int:
+            if not n:
+                return 0
+            gn = 1 if m <= n.val else 0
+            cm = max(n.val, m)
+            gn += good_nodes(n.left, cm)
+            gn += good_nodes(n.right, cm)
+            return gn
+
+        return good_nodes(root, root.val)
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)

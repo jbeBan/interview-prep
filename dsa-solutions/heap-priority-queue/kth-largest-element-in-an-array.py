4e69bc6db5fb450a68bd50fe55b1b45a0cd8e3d6
Jacob Banuelos <72123087+jbeBan@users.noreply.github.com>
2026-09-15 13:51:12 -0600

    Add kth-largest-element-in-an-array.py

---
+++ dsa-solutions/heap-priority-queue/kth-largest-element-in-an-array.py
@@ -0,0 +1,14 @@
+import heapq
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        h = []
+        for num in nums:
+            heapq.heappush(h, num)
+            if len(h) > k:
+                heapq.heappop(h)
+        return h[0]
+
+# Time Complexity: O(nlog(k))
+# Space Complexity: O(k)

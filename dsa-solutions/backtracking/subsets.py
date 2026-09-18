03a8ad860bfbc82ffe26eea136fc500d7f15a449
Jacob Banuelos <72123087+jbeBan@users.noreply.github.com>
2026-09-18 13:12:12 -0600

    Add subsets.py

---
+++ dsa-solutions/backtracking/subsets.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def helper(i, ints, c, s):
+            if i >= len(nums):
+                s.append(c.copy())
+                return
+            c.append(ints[i])
+            helper(i + 1, ints, c, s)
+            c.pop()
+            helper(i + 1, ints, c, s)
+
+        c, s = [], []
+        helper(0, nums, c, s)
+        return s
+
+
+# Time Complexity: O(2^n)
+# Space Complexity: O(n)

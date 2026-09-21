52cf1825cbf33fd638b1b45b6a1c0b633881b5fc
Jacob Banuelos <72123087+jbeBan@users.noreply.github.com>
2026-09-21 12:33:22 -0600

    Add task-scheduler.py

---
+++ dsa-solutions/heap-priority-queue/task-scheduler.py
@@ -0,0 +1,27 @@
+import heapq
+
+from collections import Counter, List, deque
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        c = Counter(tasks)
+        h = list(c.values())
+        heapq.heapify_max(h)
+        q = deque()
+        t = 0
+        while h or q:
+            t += 1
+            if not h:
+                t = q[0][1]
+            else:
+                cc = heapq.heappop_max(h) - 1
+                if cc:
+                    q.append([cc, t + n])
+            if q and q[0][1] == t:
+                heapq.heappush_max(h, q.popleft()[0])
+        return t
+
+
+# Time Complexity: O(n * m) [m: tasks length]
+# Space Complexity: O(1) [26 unique letters]
